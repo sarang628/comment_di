dd170f9d828263958f8850741c823e4ec74d5ce8
SRYANG <sarang628@naver.com>
2024-01-29 19:53:43 +0900

    update repository module

--- CommentModule.kt
+++ CommentModule.kt
@@ -1,49 +1,41 @@
 package com.sarang.torang.di.comment_di
 
 import com.sarang.torang.BuildConfig
+import com.sarang.torang.api.ApiComment
 import com.sarang.torang.data.comments.Comment
 import com.sarang.torang.data.comments.User
+import com.sarang.torang.data.dao.LoggedInUserDao
+import com.sarang.torang.repository.CommentRepository
+import com.sarang.torang.session.SessionService
 import com.sarang.torang.usecase.comments.DeleteCommentUseCase
 import com.sarang.torang.usecase.comments.GetCommentsUseCase
 import com.sarang.torang.usecase.comments.GetUserUseCase
 import com.sarang.torang.usecase.comments.SendCommentUseCase
-import com.sryang.torang_repository.api.ApiComment
-import com.sryang.torang_repository.data.dao.LoggedInUserDao
-import com.sryang.torang_repository.session.SessionService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import java.lang.Exception
-import java.text.SimpleDateFormat
 
 @InstallIn(SingletonComponent::class)
 @Module
 class CommentModule {
     @Provides
     fun providesGetCommentsUseCase(
-        apiComment: ApiComment,
-        sessionService: SessionService
+        commentRepository: CommentRepository
     ): GetCommentsUseCase {
         return object : GetCommentsUseCase {
             override suspend fun invoke(reviewId: Int): List<Comment> {
-                val auth = sessionService.getToken()
-                if (auth != null) {
-                    return apiComment.getComments(auth = auth, reviewId = reviewId).list.map {
-                        Comment(
-                            name = it.user.userName,
-                            comment = it.comment,
-                            date = it.create_date,
-                            likeCount = 0,
-                            profileImageUrl = BuildConfig.PROFILE_IMAGE_SERVER_URL + it.user.profilePicUrl,
-                            userId = it.user.userId,
-                            commentsId = it.comment_id
-                        )
-                    }
-                } else {
-                    throw Exception("로그인을 해주세요.")
+                return commentRepository.getComment(reviewId = reviewId).list.map {
+                    Comment(
+                        name = it.user.userName,
+                        comment = it.comment,
+                        date = it.create_date,
+                        likeCount = 0,
+                        profileImageUrl = BuildConfig.PROFILE_IMAGE_SERVER_URL + it.user.profilePicUrl,
+                        userId = it.user.userId,
+                        commentsId = it.comment_id
+                    )
                 }
-
             }
         }
     }
@@ -106,15 +98,5 @@ class CommentModule {
                 apiComment.deleteComment(commentId)
             }
         }
-    }
-
-    fun dateToFormat(date : String): String {
-        var format = ""
-
-        val sdf = SimpleDateFormat("yyyy-mm-dd hh:MM:ss")
-        val date = sdf.parse(date)
-
-
-        return format
     }
 }
