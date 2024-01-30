1e788d754fd7dc930afce4767cea83fedfd0efac
SRYANG <sarang628@naver.com>
2024-01-30 13:22:13 +0900

    update date convert

--- CommentModule.kt
+++ CommentModule.kt
@@ -11,6 +11,7 @@ import com.sarang.torang.usecase.comments.DeleteCommentUseCase
 import com.sarang.torang.usecase.comments.GetCommentsUseCase
 import com.sarang.torang.usecase.comments.GetUserUseCase
 import com.sarang.torang.usecase.comments.SendCommentUseCase
+import com.sarang.torang.util.DateConverter
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,7 +30,7 @@ class CommentModule {
                     Comment(
                         name = it.user.userName,
                         comment = it.comment,
-                        date = it.create_date,
+                        date = DateConverter.formattedDate(it.create_date),
                         likeCount = 0,
                         profileImageUrl = BuildConfig.PROFILE_IMAGE_SERVER_URL + it.user.profilePicUrl,
                         userId = it.user.userId,
