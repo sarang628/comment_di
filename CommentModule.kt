838ece92d52702bc0bec96e549d2ab66aca40796
SRYANG <sarang628@naver.com>
2024-02-03 12:04:53 +0900

    update comment like count

--- CommentModule.kt
+++ CommentModule.kt
@@ -2,16 +2,19 @@ package com.sarang.torang.di.comment_di
 
 import com.sarang.torang.BuildConfig
 import com.sarang.torang.api.ApiComment
+import com.sarang.torang.api.ApiCommentLike
 import com.sarang.torang.data.comments.Comment
 import com.sarang.torang.data.comments.User
 import com.sarang.torang.data.dao.LoggedInUserDao
 import com.sarang.torang.repository.CommentRepository
+import com.sarang.torang.session.SessionClientService
 import com.sarang.torang.session.SessionService
+import com.sarang.torang.usecase.comments.AddCommentLikeUseCase
+import com.sarang.torang.usecase.comments.DeleteCommentLikeUseCase
 import com.sarang.torang.usecase.comments.DeleteCommentUseCase
 import com.sarang.torang.usecase.comments.GetCommentsUseCase
 import com.sarang.torang.usecase.comments.GetUserUseCase
 import com.sarang.torang.usecase.comments.SendCommentUseCase
-import com.sarang.torang.usecase.comments.UpdateCommentLikeUseCase
 import com.sarang.torang.util.DateConverter
 import dagger.Module
 import dagger.Provides
@@ -35,7 +38,8 @@ class CommentModule {
                         profileImageUrl = BuildConfig.PROFILE_IMAGE_SERVER_URL + it.user.profilePicUrl,
                         userId = it.user.userId,
                         commentsId = it.comment_id,
-                        commentLikeCount = it.comment_like_count
+                        commentLikeCount = it.comment_like_count,
+                        commentLikeId = it.comment_like_id
                     )
                 }
             }
@@ -103,10 +107,30 @@ class CommentModule {
     }
 
     @Provides
-    fun providesUpdateCommentLikeUseCase(apiComment: ApiComment): UpdateCommentLikeUseCase {
-        return object : UpdateCommentLikeUseCase {
-            override suspend fun invoke(commentId: Int): Comment {
-                TODO("Not yet implemented")
+    fun providesAddCommentLikeUseCase(
+        apiCommentLike: ApiCommentLike,
+        sessionClientService: SessionClientService
+    ): AddCommentLikeUseCase {
+        return object : AddCommentLikeUseCase {
+            override suspend fun invoke(commentId: Int): Int {
+                val result =
+                    apiCommentLike.addCommentLike(sessionClientService.getToken() ?: "", commentId)
+                return result.commentLikeId
+            }
+        }
+    }
+
+    @Provides
+    fun providesDeleteCommentLikeUseCase(
+        apiCommentLike: ApiCommentLike,
+        sessionClientService: SessionClientService
+    ): DeleteCommentLikeUseCase {
+        return object : DeleteCommentLikeUseCase {
+            override suspend fun invoke(commentId: Int): Boolean {
+                return apiCommentLike.deleteCommentLike(
+                    sessionClientService.getToken() ?: "",
+                    commentId
+                )
             }
         }
     }
